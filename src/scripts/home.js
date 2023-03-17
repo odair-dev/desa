e7e165eaf27764719bbe331886766dd805daa4f1
Odair <odairodriguez@yahoo.com.br>
2023-03-17 11:48:48 -0300

    slider

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -18,3 +18,40 @@ iconBars.addEventListener('click', ()=>{
 liLogin.addEventListener('click', ()=>{
     window.location.replace('./login.html');
 })
+
+//********************** SLIDES **********************/
+let cont=0;
+let divSlides = document.querySelector('.divSlides');
+let divBefore = document.querySelector('.divBefore');
+let divAfter = document.querySelector('.divAfter');
+
+setInterval(()=>{
+    if(cont>300){
+        cont=0;
+    }
+    divSlides.style.margin=`0 0 0 -${cont}%`;
+    cont=cont+100;
+},6000);
+
+divBefore.addEventListener('click', ()=>{
+    if(cont==0){
+        cont=300;
+        divSlides.style.margin=`0 0 0 -${cont}%`;
+    }
+    else{
+        cont=cont-100;
+        divSlides.style.margin=`0 0 0 -${cont}%`;
+    }
+})
+
+divAfter.addEventListener('click', ()=>{
+    if(cont==300){
+        cont=0;
+        divSlides.style.margin=`0 0 0 -${cont}%`;
+    }
+    else{
+        cont=cont+100;
+        divSlides.style.margin=`0 0 0 -${cont}%`;
+    }
+})
+//****************************************************/
